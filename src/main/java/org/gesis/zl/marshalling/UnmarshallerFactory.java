dd3e336726c2fc8ff4ec826559ef99105ce13b91
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2013-02-06 17:30:18 +0100

    added factory class for creating default csv interpreter

---
+++ src/main/java/org/gesis/zl/marshalling/UnmarshallerFactory.java
@@ -0,0 +1,32 @@
+package org.gesis.zl.marshalling;
+
+import java.io.Reader;
+
+import org.gesis.zl.marshalling.annotations.AnnotationInterpreterFactory;
+import org.gesis.zl.marshalling.csv.CsvAnnotationInterpreter;
+import org.gesis.zl.marshalling.csv.CsvUnmarshaller;
+
+public class UnmarshallerFactory
+{
+
+	/**
+	 * Creates a default CsvUnmarshaller-instance with the default
+	 * CsvAnnotationInterpreter-instance. The annotation interpreter is created
+	 * by invoking the AnnotationInterpreterFactory method
+	 * <i>createDefaultCsvAnnotationInterpreter</i>.
+	 * 
+	 * @param annotatedClass
+	 * @param reader
+	 * @return
+	 */
+	public static <T> Unmarshaller<T> createDefaultCsvUnmarshaller(Class<T> annotatedClass, Reader reader)
+	{
+		// the annotation interpreter
+		CsvAnnotationInterpreter<T> interpreter = AnnotationInterpreterFactory.createDefaultCsvAnnotationInterpreter( annotatedClass );
+
+		// the csv unmarshaller
+		Unmarshaller<T> unmarshaller = new CsvUnmarshaller<T>( interpreter, reader );
+
+		return unmarshaller;
+	}
+}
